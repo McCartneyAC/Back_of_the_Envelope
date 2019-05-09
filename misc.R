982b73019db7c0df9b45894c32bbb8af307484e3
Andrew McCartney <acm9q@virginia.edu>
2019-05-08 20:22:54 -0400

    ideas and things.

---
+++ misc.R
@@ -0,0 +1,4 @@
+select2Input("select2Input1","This is a multiple select2Input. The items are re-arrangeable",
+                        choices=c("a","b","c"),
+                        selected=c("b","a"))
+         )
