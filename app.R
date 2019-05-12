f95b8ff800f754ec9e5dd605ac0d515c8fb461af
Andrew McCartney <acm9q@virginia.edu>
2019-05-12 19:22:14 -0400

    bivariate plots available

--- app.R
+++ app.R
@@ -10,31 +10,30 @@
 # select regression types? 
 # 
 # 
-
+# , 
 source("functions.R")
 
 ui <- fluidPage(
-    titlePanel("Back of the Envelope Regression"),
+    titlePanel(#title=div(img(src="stamp.jpg"),
+                           "Back of the Envelope Regression" #)
+    ),
     sidebarLayout(sidebarPanel(
         fileInput("FileInput", "Input Your Data Set (Must be .csv)"),
-        tags$br(),
-        tags$p(tags$b("Build your Model:")), 
+        wired_icon_button("button", icon = "mail_outline"),
+        tags$h3(tags$b("Build your Model:")), 
         wired_radio(
             inputId = "rgrssn", label = "Regression:",
             choices = c("linear" = "linear",
                         "logistic" = "logistic")
         ),
         wired_toggle(inputId = "rbst", label = "Robust Standard Errors"),
-        tags$p("Select your variables for analysis:"),
-        tags$p("Your DV / Response Variable:"),
+        tags$p(tags$b("Select your variables for analysis:")),
         selectInput(inputId = "responsevar",
-                     label = "Dependent Varibale:", 
+                     label = "Your DV / Response Variable:", 
                      choices = NULL
                      ),
-        # checkbox input
-        tags$p("Your IV / Predictor Variable(s):"),
 
-        selectizeInput("indevars", "Independent Variable(s)", 
+        selectizeInput("indevars", "Your IV / Predictor Variable(s):", 
                        choices = NULL, 
                        selected = NULL, 
                        # CHANGE ME LATER
@@ -47,21 +46,27 @@ ui <- fluidPage(
              choices = c("None",
                          "Fixed Effects",
                          "Cluster Standard Errors",
-                         "Multilevel Model / LME")
+                         "Multilevel Model / LME"),
+             selected = "None"
              ),
         selectInput(inputId = "clust",
                     label = "Cluster Varibale:", 
                     choices = NULL
         )
+
     ), #sidebar panel
     mainPanel(
         tabsetPanel(
             tabPanel("About",
+                     tags$h3("About"),
                      tags$p("This is intended to be a toy point-and-click-style regression tool to practice R Shiny application development and to enumerate the complexities available in regression analysis. Like R itself, this tool comes with absolutely no warranty. "),
+                     tags$h3("Use"),
                      tags$p("Use the tool by uploading your own data set in .csv format (.xlsx functionality coming soon). Browse your data and examine the variables' descriptive statistics, as well as the table of correlations, then create your model to run. "),
                      tags$p("The sketchy nature of the application is intended to deter its use for actual serious purposes and strengthen the feeling of it being a back-of-the-envelope tool for regression analysis."), 
                      tags$p(tags$b("What this app doesn't do:"), "This app does not allow for any kind of data preparation. Techniques such as interaction terms, exponential terms, or complex extensions such as regression discontinuity need to be done in whatever data-preparation program you choose to use (e.g. excel) before data can be uploaded and used here."),
-                     tags$p("Back of the Envelope was built with the following R packages: Shiny, wired, xkcd, DT, psych, SjPlot, MASS, mccrr, and the tidyverse.")
+                     tags$h3("Credit"),
+                     tags$p("Back of the Envelope was built with the following R packages: Shiny, wired, xkcd, DT, psych, SjPlot, MASS, mccrr, and the tidyverse."), 
+                     wired_icon_button("button3", icon = "mail")
                      ),
             tabPanel("Table",
                      DT::dataTableOutput("table")
@@ -72,7 +77,14 @@ ui <- fluidPage(
             tabPanel("Correlations",
                      plotOutput("cors")
                      ),
-            tabPanel("Plot"), 
+            tabPanel("Plot", 
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("Bivariate", plotOutput("bivariate")
+                                          ),
+                                 tabPanel("Multiple Regression" # , tableOutput("table_mult")
+                                          )
+                     )
+                     ), 
             tabPanel("Output Summary", 
                      tags$p("Be sure to include a null_model if LME is selected and if a cluster is chosen"),
                      htmlOutput("tabmodel")
@@ -113,7 +125,10 @@ server <- function(input, output, session) {
         psych::describe(datasetInput(), fast = T) %>% 
             mutate(mean = round(mean, 2)) %>% 
             mutate(sd = round(sd, 2)) %>% 
-            mutate(se = round(se, 2))
+            mutate(se = round(se, 2)) %>% 
+            mutate(min = round(min, 2)) %>% 
+            mutate(max = round(max, 2)) %>% 
+            mutate(range = round(range, 2))
     })
    output$description =  DT::renderDataTable(desc())
    
@@ -122,8 +137,8 @@ server <- function(input, output, session) {
        datasetInput() %>% 
            select_if(is_extant) %>% 
            select_if(is_numeric) %>% 
-           sjp.corr(data = ., sort.corr = T, decimals = 2, na.deletion = "pairwise") + 
-           theme_xkcd()
+           sjp_corr(data = ., sort.corr = T, decimals = 2, na.deletion = "pairwise", show.p = FALSE) + 
+           theme_xkcd() 
    )
    
    
@@ -158,10 +173,7 @@ server <- function(input, output, session) {
        as.formula(paste(input$responsevar, '~', feats()))
    })
    
-   output$feats <- feats
-   output$regFormula <- regFormula
-   
-   # bivariate model
+   # Linear Model
    model <- reactive({
        if (input$rbst) {
            MASS::rlm(regFormula(), data = datasetInput())
@@ -181,8 +193,34 @@ server <- function(input, output, session) {
        HTML(modeltab$knitr)
    })
 
-
    
+   xrange <- reactive({
+       datasetInput() %>% 
+       select_(input$indevars) %>% 
+       range()
+   })
+   yrange <- reactive({
+       datasetInput() %>% 
+       select_(input$responsevar) %>% 
+       range()   
+   })
+   indvariable <- reactive({
+       input$indevars
+   })
+   depvariable <- reactive({
+       input$responsevar
+   })
+   output$bivariate <- renderPlot(
+       datasetInput() %>%
+           ggplot(aes_string(x = indvariable(), y = depvariable())) +
+           geom_jitter() +
+           geom_smooth(method = "lm") +
+           theme_xkcd() +
+           xkcdaxis(xrange(), yrange())
+)
+
+
+
 } #server
 
 
--- functions.R
+++ functions.R
@@ -11,8 +11,173 @@ library(magrittr)
 library(dplyr)
 library(sjPlot)
 library(MASS)
+library(fontawesome)
 # loadfonts()
 
 is_extant <-function(x) any(!is.na(x))
 is_numeric<-function(x) any(is.numeric(x))
 
+
+sjp_corr <- function (data, title = NULL, axis.labels = NULL, sort.corr = TRUE, 
+                      decimals = 3, na.deletion = c("listwise", "pairwise"), 
+                      corr.method = c("pearson", "spearman", "kendall"), 
+                      geom.colors = "RdBu", wrap.title = 50, wrap.labels = 20, 
+                      show.legend = FALSE, legend.title = NULL, show.values = TRUE, 
+                      show.p = TRUE, p.numeric = FALSE) 
+{
+  opt <- getOption("p_zero")
+  if (is.null(opt) || opt == FALSE) {
+    p_zero <- ""
+  }
+  else {
+    p_zero <- "0"
+  }
+  na.deletion <- match.arg(na.deletion)
+  corr.method <- match.arg(corr.method)
+  if (is.null(axis.labels) && is.data.frame(data)) {
+    axis.labels <- unname(sjlabelled::get_label(data, def.value = colnames(data)))
+  }
+  if (is.brewer.pal(geom.colors[1])) {
+    geom.colors <- (scales::brewer_pal(palette = geom.colors[1]))(5)
+  }
+  else if (geom.colors[1] == "gs") {
+    geom.colors <- (scales::grey_pal())(5)
+  }
+  if (any(class(data) == "matrix")) {
+    corr <- data
+    cpvalues <- NULL
+  }
+  else {
+    if (na.deletion == "listwise") {
+      data <- stats::na.omit(data)
+      corr <- stats::cor(data, method = corr.method)
+    }
+    else {
+      corr <- stats::cor(data, method = corr.method, use = "pairwise.complete.obs")
+    }
+    computePValues <- function(df) {
+      cp <- c()
+      for (i in seq_len(ncol(df))) {
+        pv <- c()
+        for (j in seq_len(ncol(df))) {
+          test <- suppressWarnings(stats::cor.test(df[[i]], 
+                                                   df[[j]], alternative = "two.sided", 
+                                                   method = corr.method))
+          pv <- c(pv, round(test$p.value, 4))
+        }
+        cp <- rbind(cp, pv)
+      }
+      return(cp)
+    }
+    cpvalues <- computePValues(data)
+  }
+  if (is.null(axis.labels)) 
+    axis.labels <- row.names(corr)
+  if (!is.null(title)) 
+    title <- sjmisc::word_wrap(title, wrap.title)
+  if (!is.null(axis.labels)) 
+    axis.labels <- sjmisc::word_wrap(axis.labels, wrap.labels)
+  if (sort.corr) {
+    neword <- order(corr[1, ])
+    orderedCorr <- corr[neword, neword]
+    axis.labels <- axis.labels[neword]
+    if (!is.null(cpvalues)) 
+      cpvalues <- cpvalues[neword, neword]
+  }
+  else {
+    cl <- ncol(corr)
+    orderedCorr <- corr[cl:1, cl:1]
+    axis.labels <- rev(axis.labels)
+    if (!is.null(cpvalues)) 
+      cpvalues <- cpvalues[cl:1, cl:1]
+  }
+  yo <- c()
+  for (i in seq_len(nrow(corr))) {
+    yo <- c(yo, rep(i, nrow(corr)))
+  }
+  orderedCorr <- tidyr::gather(data.frame(orderedCorr), "var", 
+                               "value", !!seq_len(ncol(orderedCorr)), factor_key = TRUE)
+  if (!is.null(cpvalues)) 
+    cpvalues <- tidyr::gather(data.frame(cpvalues), "var", 
+                              "value", !!seq_len(ncol(cpvalues)), factor_key = TRUE)
+  orderedCorr <- cbind(orderedCorr, ordx = seq_len(nrow(corr)), 
+                       ordy = yo)
+  if (!is.null(cpvalues)) {
+    if (!p.numeric) {
+      cpv <- sapply(cpvalues$value, get_p_stars)
+    }
+    else {
+      cpv <- sapply(cpvalues$value, function(x) {
+        if (x < 0.001) 
+          x <- sprintf("\n(< %s.001)", p_zero)
+        else x <- sub("0", p_zero, sprintf("\n(%.*f)", 
+                                           decimals, x))
+      })
+    }
+  }
+  else {
+    cpv <- ""
+  }
+  orderedCorr$ps <- cpv
+  if (!show.values) {
+    orderedCorr$val.labels <- ""
+  }
+  else {
+    if (show.p) {
+      orderedCorr$val.labels <- sprintf("%.*f%s", 
+                                        decimals, orderedCorr$value, orderedCorr$ps)
+    }
+    else {
+      orderedCorr$val.labels <- sprintf("%.*f", decimals, 
+                                        orderedCorr$value)
+    }
+  }
+  orderedCorr$val.labels[orderedCorr$ordx >= orderedCorr$ordy] <- NA
+  orderedCorr$ordx <- as.factor(orderedCorr$ordx)
+  orderedCorr$ordy <- as.factor(orderedCorr$ordy)
+  message(sprintf("Computing correlation using %s-method with %s-deletion...", 
+                  corr.method, na.deletion))
+  corrPlot <- ggplot(orderedCorr, 
+                     aes_string(x = "ordx", 
+                                y = "ordy", 
+                                fill = "value", 
+                                label = "val.labels")) + 
+    geom_tile(size = 0, colour = "black") + 
+    scale_x_discrete(labels = axis.labels, 
+                     breaks = seq_len(length(axis.labels))) +
+    scale_y_discrete(labels = axis.labels, 
+                     breaks = seq_len(length(axis.labels))) + 
+    scale_fill_gradientn(colours = geom.colors, 
+                         limits = c(-1, 1)) + 
+    geom_text(size = 3.5, colour = "black", family = "xkcd") + 
+    labs(title = title, x = NULL, y = NULL)
+  
+  if (show.legend) 
+    corrPlot <- corrPlot + guides(fill = legend.title)
+  else corrPlot <- corrPlot + guides(fill = "none")
+  corrPlot
+}
+
+is.brewer.pal <- function(pal) {
+  bp.seq <- c("BuGn", "BuPu", "GnBu", "OrRd", "PuBu", "PuBuGn", "PuRd", "RdPu",
+              "YlGn", "YlGnBu", "YlOrBr", "YlOrRd", "Blues", "Greens", "Greys",
+              "Oranges", "Purples", "Reds")
+  bp.div <- c("BrBG", "PiYG", "PRGn", "PuOr", "RdBu", "RdGy", "RdYlBu",
+              "RdYlGn", "Spectral")
+  bp.qul <- c("Accent", "Dark2", "Paired", "Pastel1", "Pastel2", "Set1",
+              "Set2", "Set3")
+  bp <- c(bp.seq, bp.div, bp.qul)
+  pal %in% bp
+}
+get_p_stars <- function(pval, thresholds = NULL) {
+  
+  if (is.null(thresholds)) thresholds <- c(.05, .01, .001)
+  
+  dplyr::case_when(
+    is.na(pval) ~ "",
+    pval < thresholds[3] ~ "***",
+    pval < thresholds[2] ~ "**",
+    pval < thresholds[1] ~ "*",
+    TRUE ~ ""
+  )
+}
