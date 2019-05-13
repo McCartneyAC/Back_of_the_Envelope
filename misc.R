70f20be9d56a1c992979bd7c3ada6ad00507bb0a
Andrew McCartney <acm9q@virginia.edu>
2019-05-13 10:17:29 -0400

    initial take on logistic model support

--- misc.R
+++ misc.R
@@ -5,3 +5,25 @@ fa("chart-line", fill = "steelblue")
 fa("chart-bar", fill = "steelblue")
 fa("chart-area", fill = "steelblue")
 fa("r-project", fill = "steelblue")
+
+
+# Model Building
+linear <- reactive ({
+  if (input$rbst) {
+                MASS::rlm(regFormula(), data = datasetInput())
+            } else {
+                lm(regFormula(), data = datasetInput())
+    }
+  })
+          
+logistic <- reactive({
+    glm(regFormula(), data = datasetInput(), family = "binomial")
+})
+          
+model <- reactive({
+  if (input$rgrssn == "logistic") {
+    logistic()
+    } else {
+    linear()
+    }
+})
