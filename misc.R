b2817b9a0ee37d82409a3f5f67f604bf4198dc06
Andrew McCartney <acm9q@virginia.edu>
2019-05-13 07:35:38 -0400

    Update misc.R

--- misc.R
+++ misc.R
@@ -1,21 +1,3 @@
-select2Input("select2Input1","This is a multiple select2Input. The items are re-arrangeable",
-                        choices=c("a","b","c"),
-                        selected=c("b","a"))
-         )
-
-
-f()<-reactive({
-  as.formula(paste0(input$dependentvar, " ~ ", input$indevar))
-  })
-model<-reactive({
-  if (robust = FALSE){
-  lm(f(), data = dataInput())
-    }
-  else {
-    rlm(f(), data = dataInput())
-    }
-  })
-
 fa("envelope", fill = "steelblue")
 fa("envelope-open", fill = "steelblue")
 fa("envelope-open-text", fill = "steelblue")
@@ -23,8 +5,3 @@ fa("chart-line", fill = "steelblue")
 fa("chart-bar", fill = "steelblue")
 fa("chart-area", fill = "steelblue")
 fa("r-project", fill = "steelblue")
-
-# robust:
-library(MASS)
-rlm_mod <- rlm(stack.loss ~ ., stackloss, psi = psi.bisquare)  # robust reg model
-summary(rlm_mod)
